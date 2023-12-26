7b14b75ffa9cba82e0a55ead90d646932fbffd50
dctalbot <David.Talbot@superconductive.com>
2023-12-26 00:13:08 -0500

    rm unused checked

--- src/Keyboard/KeyboardKey.tsx
+++ src/Keyboard/KeyboardKey.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import { Key } from "../lib/useKeyboard";
 import { useTone } from "../lib/useTone";
 
@@ -15,31 +14,20 @@ function KeyboardKey({
   variant,
 }: KeyboardKeyProps) {
   const tone = useTone({ freq: keyConfig.freq });
-  const [checked, setChecked] = useState(false);
 
   const className2 =
     variant === "white"
       ? ` flex-1 bg-neutral-50 hover:bg-neutral-200 border border-black `
       : ` bg-neutral-950 hover:bg-neutral-700 border border-black `;
 
-  useEffect(() => {
-    if (checked) {
-      tone.play();
-    } else {
-      tone.stop();
-    }
-  }, [checked]);
-
   return (
     <div
       className={className + className2}
       style={style}
       onMouseEnter={() => {
-        if (checked) return;
         tone.play();
       }}
       onMouseLeave={() => {
-        if (checked) return;
         tone.stop();
       }}
     ></div>
