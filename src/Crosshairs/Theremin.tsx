d652654e8ce3f8adb4481cc7e30086fc2c8a8c46
dctalbot <David.Talbot@superconductive.com>
2023-12-25 23:02:21 -0500

    wip

--- src/Crosshairs/Theremin.tsx
+++ src/Crosshairs/Theremin.tsx
@@ -1,19 +1,19 @@
 import { Crosshairs } from "./Crosshairs";
 
-function Theremin() {
-  const makeToneConfig = ({
-    rect,
-    mouse,
-  }: {
-    rect: DOMRect;
-    mouse: { x: number; y: number };
-  }) => {
-    return {
-      freq: mouse.x * 2,
-      volume: mouse.y && rect.height ? (1 - mouse.y / rect.height) * 2 : 1,
-    };
+const makeToneConfig = ({
+  rect,
+  mouse,
+}: {
+  rect: DOMRect;
+  mouse: { x: number; y: number };
+}) => {
+  return {
+    freq: mouse.x * 2,
+    volume: mouse.y && rect.height ? (1 - mouse.y / rect.height) * 2 : 1,
   };
+};
 
+function Theremin() {
   return <Crosshairs makeToneConfig={makeToneConfig} />;
 }
 
