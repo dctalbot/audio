de200e16a2ae24842769097c8402080383c75bc1
dctalbot <David.Talbot@superconductive.com>
2023-12-26 00:23:06 -0500

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import "./App.css";
 import { KeyboardDemo } from "./demos/KeyboardDemo";
 import { CrosshairsDemo } from "./demos/CrosshairsDemo";
+import { Trombone } from "./Crosshairs/Trombone";
+import { Theremin } from "./Crosshairs/Theremin";
 
 function App() {
   return (
     <div className="some-div p-2 gap-1 flex flex-col">
       <button>click me first</button>
       <KeyboardDemo />
-      <CrosshairsDemo />
+      <Trombone />
+      <Theremin />
     </div>
   );
 }
